40175caef41a316fbeccb353c3e6d36bf1a8b3f4
SUN-AAA <seouna06@gmail.com>
2026-02-09 18:32:01 +0900

    spring boot project file upload

---
+++ devsign-backend/src/main/java/kr/co/devsign/devsign_backend/DevsignBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co.devsign.devsign_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsignBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsignBackendApplication.class, args);
+	}
+
+}
